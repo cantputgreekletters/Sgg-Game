6a16bbccc1dffe847c1d4caa208007f7a6a5c81a
cantputgreekletters <121383375+cantputgreekletters@users.noreply.github.com>
2024-12-02 19:16:04 +0200

    Added dependencies

--- make.bat
+++ make.bat
@@ -1,3 +1,11 @@
 @echo off
-g++ main.cpp ./sgg/*.cpp ./include/*.cpp -DGLM_ENABLE_EXPERIMENTAL -I. -I"L:/Programming Languages/Languages/MySys2/ucrt64/include/freetype2"  -lopengl32 -lglew32 -lSDL2 -lSDL2_mixer -lfreetype -o main
-echo "Runned compilation"
+g++ main.cpp ./sgg/*.cpp ./include/*.cpp ./dependencies/*.dll -DGLM_ENABLE_EXPERIMENTAL -I. -I./freetype2 -o main
+
+if errorlevel 1 (
+    echo Compilation failed!
+    pause
+    exit /b 1
+) else (
+    echo Compilation successful!
+    pause
+)
